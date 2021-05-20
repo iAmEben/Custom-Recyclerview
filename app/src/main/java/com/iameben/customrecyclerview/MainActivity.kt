db1cf21e847892a78ba43eaca6079167aa22247a
Ebenezer Justice <justiceebenezer4@gmail.com>
2021-05-20 04:36:36 +0100

    init

--- app/src/main/java/com/iameben/customrecyclerview/MainActivity.kt
+++ app/src/main/java/com/iameben/customrecyclerview/MainActivity.kt
@@ -2,10 +2,20 @@ package com.iameben.customrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.iameben.customrecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val adapter = ContactAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpData(binding)
+    }
+
+    fun setUpData(binding: ActivityMainBinding){
+        binding.contactsRV.adapter = adapter
+
     }
 }
