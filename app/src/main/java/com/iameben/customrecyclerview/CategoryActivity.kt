008f1de338cff141f9a4292d73fa029152decf4f
Ebenezer Justice <justiceebenezer4@gmail.com>
2021-05-21 22:36:36 +0100

    init for login and category activities

--- app/src/main/java/com/iameben/customrecyclerview/CategoryActivity.kt
+++ app/src/main/java/com/iameben/customrecyclerview/CategoryActivity.kt
@@ -1,11 +1,34 @@
 package com.iameben.customrecyclerview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class CategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
+    }
+// The following functions send intents from the cardview of the activity
+    fun allContacts(view: View) {
+        val intent = Intent(this, AllContactActivity::class.java)
+        startActivity(intent)
+
+    }
+
+    fun favourites(view: View) {
+        val intent = Intent(this, FavouriteContactActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun family(view: View) {
+        val intent = Intent(this, FamilyContactActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun friends(view: View) {
+        val intent = Intent(this, FriendsContactActivity::class.java)
+        startActivity(intent)
     }
 }
